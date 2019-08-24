3e1c890a81485227cdaf4943effd95d0e022aec7
Alex Pekker <alp2003@gmail.com>
2019-08-24 16:08:43 +0300

    Layouts Kinds
    Added some layouts examples

--- app/src/main/java/com/android/androidtutorial004/MainActivity.java
+++ app/src/main/java/com/android/androidtutorial004/MainActivity.java
@@ -1,5 +1,8 @@
 package com.android.androidtutorial004;
 
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
